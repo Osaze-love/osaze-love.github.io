e964d18f317befd09d02df9294532f7c5e124f02
Osaze-Omobude Love <loveomobude23@gmail.com>
2022-02-25 11:52:06 +0100

    made changes

--- EBH/main.js
+++ EBH/main.js
@@ -5,7 +5,7 @@ document.querySelector("body").classList.add("spinner-2");
 setTimeout(() => {
   document.querySelector("body").classList.remove("spinner-2");
   document.querySelector(".contents").style.display = "block";
-}, 3000);
+}, 2000);
 
 const hamburger = document.querySelector(".hamburger");
 const Navul = document.querySelector(".navUl");
